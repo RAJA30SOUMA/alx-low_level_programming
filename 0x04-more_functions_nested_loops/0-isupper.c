4356cad8161b6337d9e76c1e06e044e57767f202
RAJA30SOUMA <asmaa.matlaya123@gmail.com>
2023-03-23 08:00:26 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,14 @@
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
  */
-int _isupper(int x)
-{ 
-	if (x >= 65 && x <= 90)
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
         { 
 	return (1);
-	}       
+	else
+	{	       
 	return (0);
+	}
 	
 }	
