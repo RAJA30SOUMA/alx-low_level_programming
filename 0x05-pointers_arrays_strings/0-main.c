9a1b7da508702a8e68cdd3cd8c0da32bc40e5fa9
RAJA30SOUMA <asmaa.matlaya123@gmail.com>
2023-03-27 06:46:39 -0700

    98 Battery st

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,18 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include "main.h"
 #include <stdio.h>
 
 /**
- 
- * main - check the code 
+ * main - check the code
  *
  * Return: Always 0.
-    
  */
 int main(void)
 {
-    int n;
+	 int n;
 
-    n = 402;
+	n = 402;
     printf("n=%d\n", n);
     reset_to_98(&n);
     printf("n=%d\n", n);
