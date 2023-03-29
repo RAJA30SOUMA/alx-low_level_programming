088514e02d58f546adc045f6258a4156c17e98d0
RAJA30SOUMA <asmaa.matlaya123@gmail.com>
2023-03-29 16:47:40 -0700

    hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,10 +4,9 @@ int main(void)
 {
   int n;
   int a[5];
-  int *p;
-
-   a[2] = 1024;
-   p = &n;
+  int *p
+  a[2] = 1024;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -21,22 +20,5 @@ int main(void)
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
